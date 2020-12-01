97778b28ed882fabbd5d992d4a5ddd60b314f0af
Stefan Scheidt <stefan.scheidt@rewe-digital.com>
2020-12-01 10:51:31 +0100

    minor refactorings

--- src/main/kotlin/day01/Day01.kt
+++ src/main/kotlin/day01/Day01.kt
@@ -4,17 +4,19 @@ package day01
 fun processLines1(lines: Sequence<String>): Int {
     val ints = lines.toListOfInt()
     val combinations = ints.flatMap { i -> ints.map { j -> listOf(i, j) } }
-    return combinations
-        .find { it.sum() == 2020 }
-        ?.reduce { acc, i -> acc * i } ?: 0
+    return combinations.eval()
 }
 
 fun processLines2(lines: Sequence<String>): Int {
     val ints = lines.toListOfInt()
     val combinations = ints.flatMap { i -> ints.flatMap { j -> ints.map { k -> listOf(i, j, k) } } }
-    return combinations
-        .find { it.sum() == 2020 }
-        ?.reduce { acc, i -> acc * i } ?: 0
+    return combinations.eval()
 }
 
-private fun Sequence<String>.toListOfInt() = this.map(String::toInt).toList()
+private fun Sequence<String>.toListOfInt(): List<Int> =
+    this.map(String::toInt).toList()
+
+private fun List<List<Int>>.eval(): Int =
+    this
+        .find { it.sum() == 2020 }
+        ?.reduce { acc, i -> acc * i } ?: 0
