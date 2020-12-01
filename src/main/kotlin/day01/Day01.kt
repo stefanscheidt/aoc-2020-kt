967f5ef5f6814b6da6291bbae021e09dbbd489cf
Stefan Scheidt <stefan.scheidt@rewe-digital.com>
2020-12-01 13:08:43 +0100

    optimisation

--- src/main/kotlin/day01/Day01.kt
+++ src/main/kotlin/day01/Day01.kt
@@ -2,15 +2,11 @@ package day01
 
 
 fun processLines1(lines: Sequence<String>): Int {
-    val ints = lines.toListOfInt()
-    val combinations = ints.flatMap { i -> ints.map { j -> listOf(i, j) } }
-    return combinations.eval()
+    return pairs(lines.toListOfInt()).eval()
 }
 
 fun processLines2(lines: Sequence<String>): Int {
-    val ints = lines.toListOfInt()
-    val combinations = ints.flatMap { i -> ints.flatMap { j -> ints.map { k -> listOf(i, j, k) } } }
-    return combinations.eval()
+    return tripples(lines.toListOfInt()).eval()
 }
 
 private fun Sequence<String>.toListOfInt(): List<Int> =
@@ -20,3 +16,24 @@ private fun List<List<Int>>.eval(): Int =
     this
         .find { it.sum() == 2020 }
         ?.reduce { acc, i -> acc * i } ?: 0
+
+internal fun pairs(xs: List<Int>): List<List<Int>> {
+    if (xs.size < 2) return emptyList()
+
+    val pairs = mutableListOf<List<Int>>()
+    xs.forEachIndexed { index, x ->
+        pairs.addAll(xs.drop(index + 1).map { y -> listOf(y, x) })
+    }
+    return pairs.toList()
+}
+
+internal fun tripples(xs: List<Int>): List<List<Int>> {
+    if (xs.size < 3) return emptyList()
+
+    val tripples = mutableListOf<List<Int>>()
+    xs.forEachIndexed { index, x ->
+        val pairs = pairs(xs.drop(index + 1))
+        tripples.addAll(pairs.map { p -> p + x })
+    }
+    return tripples.toList()
+}
