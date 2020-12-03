abed186293899f734d631ceeae0c597d7954c3a9
Stefan Scheidt <stefan.scheidt@rewe-digital.com>
2020-12-03 21:40:12 +0100

    day 3, part 1

--- src/main/kotlin/day03/Day03.kt
+++ src/main/kotlin/day03/Day03.kt
@@ -1,3 +1,25 @@
 package day03
 
+fun processWorld1(world: World): Int {
+    val maxX = world.count() - 1
+    val path = path(maxX)
+    return path.filter { pos -> world.hasTreeAt(pos) }.count()
+}
+
+typealias World = List<String>
+
+fun World.hasTreeAt(pos: Pos): Boolean {
+    val width = this[0].count()
+    return this[pos.x][pos.y % width] == '#'
+}
+
+data class Pos(
+    val x: Int,
+    val y: Int
+)
+
+fun path(maxX: Int): List<Pos> =
+    generateSequence(Pos(0, 0)) { pos -> Pos(pos.x + 1, pos.y + 3) }
+        .takeWhile { pos -> pos.x <= maxX }
+        .toList()
 
--- src/test/kotlin/day03/Day03Test.kt
+++ src/test/kotlin/day03/Day03Test.kt
@@ -1,5 +1,43 @@
 package day03
 
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+import java.io.File
+import java.nio.charset.StandardCharsets
+
 
 class Day03Test {
+
+    @Test
+    fun `process sample 1`() {
+        val world = """
+            ..##.......
+            #...#...#..
+            .#....#..#.
+            ..#.#...#.#
+            .#...##..#.
+            ..#.##.....
+            .#.#.#....#
+            .#........#
+            #.##...#...
+            #...##....#
+            .#..#...#.#
+        """.trimIndent()
+        val result = processWorld1(world.lines())
+
+        result shouldBe 7
+    }
+
+    @Test
+    fun `process input 1`() {
+        val result = File("./input/day03.txt")
+            .useLines(StandardCharsets.UTF_8) { processWorld1(it.toList()) }
+        result shouldBe 294
+    }
+
+    @Test
+    fun `compute path`() {
+        val path = path(5) // for a world with 6 rows
+        path shouldBe listOf(Pos(0, 0), Pos(1, 3), Pos(2, 6), Pos(3, 9), Pos(4, 12), Pos(5, 15))
+    }
 }
