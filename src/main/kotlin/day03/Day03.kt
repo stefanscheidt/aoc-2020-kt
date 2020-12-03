8f3309cb756bce409fc8bb0a15860d8f25eabc79
Stefan Scheidt <stefan.scheidt@rewe-digital.com>
2020-12-03 22:04:09 +0100

    day 3, part 2, WIP

--- src/main/kotlin/day03/Day03.kt
+++ src/main/kotlin/day03/Day03.kt
@@ -1,8 +1,16 @@
 package day03
 
-fun processWorld1(world: World): Int {
+fun processWorld1(world: World): Int =
+    treeCount(world, Slope(1, 3))
+
+fun processWorld2(world: World): Int =
+    listOf(Slope(1, 1), Slope(1, 3), Slope(1, 5), Slope(1, 7), Slope(2, 1))
+        .map { treeCount(world, it) }
+        .fold(1) { acc, cur -> acc * cur }
+
+internal fun treeCount(world: World, slope: Slope): Int {
     val maxX = world.count() - 1
-    val path = path(maxX)
+    val path = path(slope, maxX)
     return path.filter { pos -> world.hasTreeAt(pos) }.count()
 }
 
@@ -18,8 +26,14 @@ data class Pos(
     val y: Int
 )
 
-fun path(maxX: Int): List<Pos> =
-    generateSequence(Pos(0, 0)) { pos -> Pos(pos.x + 1, pos.y + 3) }
+operator fun Pos.plus(slope: Slope): Pos =
+    Pos(this.x + slope.x, this.y + slope.y)
+
+typealias Slope = Pos
+
+fun path(slope: Slope, maxX: Int): List<Pos> {
+    return generateSequence(Pos(0, 0)) { pos -> pos + slope }
         .takeWhile { pos -> pos.x <= maxX }
         .toList()
+}
 
--- src/test/kotlin/day03/Day03Test.kt
+++ src/test/kotlin/day03/Day03Test.kt
@@ -32,12 +32,63 @@ class Day03Test {
     fun `process input 1`() {
         val result = File("./input/day03.txt")
             .useLines(StandardCharsets.UTF_8) { processWorld1(it.toList()) }
+
         result shouldBe 294
     }
 
     @Test
+    fun `process sample 2`() {
+        val world = """
+            ..##.......
+            #...#...#..
+            .#....#..#.
+            ..#.#...#.#
+            .#...##..#.
+            ..#.##.....
+            .#.#.#....#
+            .#........#
+            #.##...#...
+            #...##....#
+            .#..#...#.#
+        """.trimIndent()
+        val result = processWorld2(world.lines())
+
+        result shouldBe 336
+    }
+
+    @Test
+    fun `process input 2`() {
+        val result = File("./input/day03.txt")
+            .useLines(StandardCharsets.UTF_8) { processWorld2(it.toList()) }
+
+        result shouldBe 1479596954
+    }
+
+    @Test
     fun `compute path`() {
-        val path = path(5) // for a world with 6 rows
+        val path = path(Slope(1, 3), 5) // for a world with 6 rows
         path shouldBe listOf(Pos(0, 0), Pos(1, 3), Pos(2, 6), Pos(3, 9), Pos(4, 12), Pos(5, 15))
+    }
+
+    @Test
+    fun `tree count`() {
+        val world = """
+            ..##.......
+            #...#...#..
+            .#....#..#.
+            ..#.#...#.#
+            .#...##..#.
+            ..#.##.....
+            .#.#.#....#
+            .#........#
+            #.##...#...
+            #...##....#
+            .#..#...#.#
+        """.trimIndent()
+
+        val counts = listOf(Slope(1, 1), Slope(1, 3), Slope(1, 5), Slope(1, 7), Slope(2, 1))
+            .map { treeCount(world.lines(), it) }
+
+        counts shouldBe listOf(2, 7, 3, 4, 2)
     }
 }
