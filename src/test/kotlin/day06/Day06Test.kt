9cda77acf8c3c9edc833a57aafc58e29d2d727ca
Stefan Scheidt <stefan.scheidt@rewe-digital.com>
2020-12-07 08:18:26 +0100

    day 6, WIP

---
+++ src/test/kotlin/day06/Day06Test.kt
@@ -0,0 +1,69 @@
+package day06
+
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+import java.io.File
+
+val sample: String =
+    """
+        abc
+
+        a
+        b
+        c
+
+        ab
+        ac
+
+        a
+        a
+        a
+        a
+
+        b
+    """.trimIndent()
+
+class Day06Test {
+
+    @Test
+    fun `process sample 1`() {
+        processText1(sample) shouldBe 11
+    }
+
+    @Test
+    fun `process text 1`() {
+        val text = File("./input/day06.txt").readText()
+
+        processText1(text) shouldBe 6748
+    }
+
+    @Test
+    fun `process sample 2`() {
+        processText2(sample) shouldBe 6
+    }
+
+    @Test
+    fun `process text 2`() {
+        val text = File("./input/day06.txt").readText()
+
+        processText2(text) shouldBe 3431
+    }
+
+}
+
+fun processText1(text: String): Int =
+    text.split("\n\n")
+        .map { group ->
+            group.toSet().count { c -> c.isLetter() }
+        }
+        .sum()
+
+fun processText2(text: String): Int =
+    text.split("\n\n")
+        .map { group ->
+            group.split("\n")
+                .map(String::toSet)
+                .reduce { acc, set -> acc.intersect(set) }
+                .count()
+        }
+        .sum()
