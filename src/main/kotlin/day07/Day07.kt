f0247c4559391f967e45f9d3ec5bd50eacf4d12f
Stefan Scheidt <stefan.scheidt@rewe-digital.com>
2021-01-03 16:22:13 +0100

    Day 7, part 2.
    Not yet tail recursive.

---
+++ src/main/kotlin/day07/Day07.kt
@@ -0,0 +1,57 @@
+package day07
+
+
+typealias Bag = String
+
+data class Content(val bag: Bag, val quantity: Int)
+
+data class Rule(val bag: Bag, val content: List<Content>)
+
+fun parseRule(input: String): Rule {
+    val split = input.dropLast(1).replace("bags", "bag").split(" contain ")
+    val bag = split[0]
+    val content = split[1].split(", ")
+        .mapNotNull { """^(\d+) ([a-zA-Z ]+)$""".toRegex().matchEntire(it) }
+        .map {
+            val (q, b) = it.destructured
+            Content(b, q.toInt())
+        }
+    return Rule(bag, content)
+}
+
+fun parseRules(input: List<String>): List<Rule> =
+    input.map(::parseRule)
+
+// Part 1
+
+typealias Ruleset1 = Map<Bag, List<Bag>>
+
+fun containers(bag: Bag, ruleset: Ruleset1): Set<Bag> =
+    ruleset.keys.filter { ruleset[it]?.contains(bag) ?: false }.toSet()
+
+fun containers(bags: Set<Bag>, ruleset: Ruleset1): Set<Bag> =
+    bags.flatMap { bag -> containers(bag, ruleset) }.toSet()
+
+fun allContainers(bag: Bag, ruleset: Ruleset1): Set<Bag> {
+    tailrec fun go(ruleset: Ruleset1, acc: Pair<Set<Bag>, Set<Bag>>): Pair<Set<Bag>, Set<Bag>> {
+        val (current, sum) = acc
+        val next = containers(current, ruleset)
+        return when {
+            next.isEmpty() -> Pair(emptySet(), sum)
+            else -> go(ruleset, Pair(next, sum + next))
+        }
+    }
+
+    val init = Pair(setOf(bag), emptySet<Bag>())
+    return go(ruleset, init).second
+}
+
+// Part 2
+
+typealias Ruleset2 = Map<Bag, List<Content>>
+
+fun totalQuantityOf(bag: Bag, ruleset: Ruleset2): Int {
+    return ruleset[bag]?.sumOf {
+        it.quantity + it.quantity * totalQuantityOf(it.bag, ruleset)
+    } ?: 0
+}
--- src/test/kotlin/day07/Day07Test.kt
+++ src/test/kotlin/day07/Day07Test.kt
@@ -6,7 +6,7 @@ import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
 import java.io.File
 
-val sample1 = """
+val sample = """
 light red bags contain 1 bright white bag, 2 muted yellow bags.
 dark orange bags contain 3 bright white bags, 4 muted yellow bags.
 bright white bags contain 1 shiny gold bag.
@@ -16,7 +16,7 @@ dark olive bags contain 3 faded blue bags, 4 dotted black bags.
 vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
 faded blue bags contain no other bags.
 dotted black bags contain no other bags.            
-""".trimIndent()
+""".trimIndent().lines()
 
 class Day07Test {
 
@@ -32,10 +32,10 @@ class Day07Test {
 
     @Test
     fun `process sample 1`() {
-        val ruleset = parseRules(sample1)
-            .associate { rule -> Pair(rule.bag, rule.content.map(Content::bag)) }
+        val ruleset = parseRules(sample)
+            .associate { Pair(it.bag, it.content.map(Content::bag)) }
 
-        allContainers(ruleset, "shiny gold bag") shouldContainExactlyInAnyOrder listOf(
+        allContainers("shiny gold bag", ruleset) shouldContainExactlyInAnyOrder listOf(
             "bright white bag",
             "muted yellow bag",
             "light red bag",
@@ -45,54 +45,46 @@ class Day07Test {
 
     @Test
     fun `process input 1`() {
-        val input = File("./input/day07.txt").readText()
+        val input = File("./input/day07.txt").readLines().filter(String::isNotEmpty)
 
         val ruleset = parseRules(input)
-            .associate { rule -> Pair(rule.bag, rule.content.map(Content::bag)) }
+            .associate { Pair(it.bag, it.content.map(Content::bag)) }
 
-        allContainers(ruleset, "shiny gold bag") shouldHaveSize 337
-    }
-}
-
-typealias Bag = String
-
-data class Content(val bag: Bag, val quantity: Int)
-
-data class Rule(val bag: Bag, val content: List<Content>)
-
-fun parseRule(input: String): Rule {
-    val split = input.dropLast(1).replace("bags", "bag").split(" contain ")
-    val bag = split[0]
-    val content = split[1].split(", ")
-        .mapNotNull { """^(\d+) ([a-zA-Z ]+)$""".toRegex().matchEntire(it) }
-        .map {
-            val (q, b) = it.destructured
-            Content(b, q.toInt())
-        }
-    return Rule(bag, content)
-}
-
-fun parseRules(input: String): List<Rule> =
-    input.lines().filter(String::isNotEmpty).map(::parseRule)
-
-typealias Ruleset = Map<Bag, List<Bag>>
-
-fun containers(ruleset: Ruleset, bag: Bag): Set<Bag> =
-    ruleset.keys.filter { ruleset[it]?.contains(bag) ?: false }.toSet()
-
-fun containers(ruleset: Ruleset, bags: Set<Bag>): Set<Bag> =
-    bags.flatMap { bag -> containers(ruleset, bag) }.toSet()
-
-fun allContainers(ruleset: Ruleset, bag: Bag): Set<Bag> {
-    tailrec fun go(ruleset: Ruleset, acc: Pair<Set<Bag>, Set<Bag>>): Pair<Set<Bag>, Set<Bag>> {
-        val (current, sum) = acc
-        val next = containers(ruleset, current)
-        return when {
-            next.isEmpty() -> Pair(emptySet(), sum)
-            else -> go(ruleset, Pair(next, sum + next))
-        }
+        allContainers("shiny gold bag", ruleset) shouldHaveSize 337
     }
 
-    val init = Pair(setOf(bag), emptySet<Bag>())
-    return go(ruleset, init).second
+    @Test
+    fun `process sample 2`() {
+        val ruleset = parseRules(sample)
+            .associate { Pair(it.bag, it.content) }
+
+        totalQuantityOf("shiny gold bag", ruleset) shouldBe 32
+    }
+
+    @Test
+    fun `process sample 2, 2`() {
+        val input = """
+            shiny gold bags contain 2 dark red bags.
+            dark red bags contain 2 dark orange bags.
+            dark orange bags contain 2 dark yellow bags.
+            dark yellow bags contain 2 dark green bags.
+            dark green bags contain 2 dark blue bags.
+            dark blue bags contain 2 dark violet bags.
+            dark violet bags contain no other bags.
+        """.trimIndent().lines()
+        val ruleset = parseRules(input)
+            .associate { Pair(it.bag, it.content) }
+
+        totalQuantityOf("shiny gold bag", ruleset) shouldBe 126
+    }
+
+    @Test
+    fun `process input 2`() {
+        val input = File("./input/day07.txt").readLines().filter(String::isNotEmpty)
+
+        val ruleset = parseRules(input)
+            .associate { Pair(it.bag, it.content) }
+
+        totalQuantityOf("shiny gold bag", ruleset) shouldBe 50100
+    }
 }
